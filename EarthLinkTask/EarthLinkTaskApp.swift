cedc32985af524e4423f67c3b686e64791abfa7c
Muhammad Kamal <mohamed.kamal@rpmanetworks.com>
2023-08-18 12:49:32 +0300

    Initial Commit

---
+++ EarthLinkTask/EarthLinkTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLinkTaskApp.swift
+//  EarthLinkTask
+//
+//  Created by Muhammad Kamal on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthLinkTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
